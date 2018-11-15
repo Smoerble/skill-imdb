9c5f696106e99346b1895d49ff4dc02c2cec33b7
Pito.Eniko <eniko.pito@vodafone.com>
2018-11-15 22:03:16 +0100

    try to print something

--- __init__.py
+++ __init__.py
@@ -40,6 +40,7 @@ class ImdbSkill(MycroftSkill):
         # Mycroft will randomly speak one of the lines from the file
         #    dialogs/en-us/imdb.dialog
         self.speak_dialog("imdb")
+        LOG.info(message.data.get('utterance'))		
 
     # The "stop" method defines what Mycroft does when told to stop during
     # the skill's execution. In this case, since the skill's functionality
